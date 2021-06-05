c523ebd669c9d843c89847d0e1e6ddbdae0c3291
Mike <63655075+MixelTe@users.noreply.github.com>
2021-06-05 14:35:57 +0300

    update engine

--- ts/TextGameEngine.ts
+++ ts/TextGameEngine.ts
@@ -1,4 +1,4 @@
-const version = "1.3";
+const version = "1.3.1";
 /** Engine for text games*/
 export class TextGameEngine
 {
@@ -35,7 +35,9 @@ export class TextGameEngine
 			this.createPopup(titles),
 		]);
 		themeDiv.appendChild(this.createThemeSwitch());
+		this.waitDiv.appendChild(Div("TextGameEngine-wait-arrowRight"));
 		this.waitDiv.appendChild(Div("TextGameEngine-wait-text", [this.styles.style(titles.tapToCon)]));
+		this.waitDiv.appendChild(Div("TextGameEngine-wait-arrowLeft"));
 		for (let i = 0; i < 3; i++) this.waitDiv.appendChild(Div("TextGameEngine-wait-bubble"));
 
 		if (appendToBody) document.body.appendChild(this.mainDiv);
