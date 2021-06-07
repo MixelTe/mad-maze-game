fc0cf56bbaa4687d030ebbb7ba1beb49d964e5ba
Mike <63655075+MixelTe@users.noreply.github.com>
2021-06-07 15:10:07 +0300

    remove 'server check'

--- ts/main.ts
+++ ts/main.ts
@@ -196,7 +196,11 @@ function createDescription()
 	const span2 = document.createElement("span");
 	div2.appendChild(span2);
 	span2.innerText = ", то что вы напишете может быть добавленно в следующей версии игры.";
-	checkServer(a1);
+
+	const div3 = document.createElement("div");
+	infDiv.appendChild(div3);
+	div3.innerText = "Если сайт не работает - сообщите автору.";
+	div3.style.color = "gray";
 
 	const a = document.createElement("a");
 	infDiv.appendChild(a);
@@ -215,23 +219,6 @@ function createDescription()
 	path.setAttribute("d", "M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z");
 	svg.style.height = "1.5em";
 	svg.style.marginTop = "10px";
-}
-
-function checkServer(a: HTMLAnchorElement)
-{
-	var img = document.createElement('img');
-	img.src = "http://mixel.somee.com/favicon.png";
-	img.addEventListener("load", () =>
-	{
-		document.body.removeChild(img)
-	});
-	img.addEventListener("error", () =>
-	{
-		document.body.removeChild(img);
-		a.href = "./contentCreator";
-	});
-    img.style.display='none';
-    document.body.appendChild(img);
 }
 
 function addName(name: string)
