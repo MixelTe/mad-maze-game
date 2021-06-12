284d37577668c656ae2abfadf921bd5d89c22864
Mike <63655075+MixelTe@users.noreply.github.com>
2021-06-12 12:24:38 +0300

    text corrected

--- ts/main.ts
+++ ts/main.ts
@@ -23,7 +23,7 @@ main();
 // new Tests(tge, runEvent);
 // new Tests(tge, runEvent).testEvent(Events[0]);
 // new Tests(tge, runEvent).printEvent(Events[0]);
-// new Tests(tge, runEvent).printEvents(Events, 1);
+// new Tests(tge, runEvent).printEvents(Events, 20);
 // NextRoom[0](tge);
 // EndesOfMaze[0](tge);
 
